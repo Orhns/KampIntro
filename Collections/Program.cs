639a1fff485af5fd5dec1f86281d9b91e83f5b86
Orhun <78608829+Orhns@users.noreply.github.com>
2023-07-08 00:07:31 +0300

    end of part 4

---
+++ Collections/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "Engin", "Murat", "Kerem", "Halil" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+
+            List<string> isimler2 = new List<string> { "Engin", "Murat", "Kerem", "Halil" };
+            isimler2.Add("Ilker");
+            foreach (var item in isimler2)
+            {
+                Console.WriteLine(item); 
+            }
+        }
+    }
+}
